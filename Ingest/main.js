ed273c8eb4f01a4c1d3dd6e55938f825ea6bb6ad
Aneesh <aneeshpatne@gmail.com>
2025-10-08 07:26:58 +0000

    Refactor data insertion logic to use Prisma client for weather data

--- Ingest/main.js
+++ Ingest/main.js
@@ -1,5 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const res = await fetch("http://192.168.1.50/sensors_v2");
 
 const data = await res.json();
 
-console.log(data);
+const record = await prisma.weather_db.create({
+  data: {
+    temperature: data.temp_c,
+    humidity: humidity_pct,
+  },
+});
+
+console.log("Inserted:", record);
