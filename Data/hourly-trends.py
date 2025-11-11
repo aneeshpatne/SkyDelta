2bed2afd9685975478699a364df460d7ecf3cc88
Aneesh <aneeshpatne@gmail.com>
2025-11-11 10:40:34 +0000

    Refactor main function to ensure proper execution of data loading for the last 30 days

--- Data/hourly-trends.py
+++ Data/hourly-trends.py
@@ -33,3 +33,8 @@ async def load_last_30_days_data():
         }
     )
     logging.info(f"Retrived Data: {len(data)}")
+
+async def main():
+    await load_last_30_days_data()
+
+asyncio.run(main())
